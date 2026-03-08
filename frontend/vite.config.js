bb47b2999e363aca3cd005e94978ef9e32eba617
Rejinold Johnson <129836048+RejinoldJohnson@users.noreply.github.com>
2026-03-08 19:00:17 +0530

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   server: {
-    historyApiFallback: true,   // dev server: serve index.html for all routes
+    port: 5173,
+  },
+  preview: {
+    port: 4173,
   },
 })
